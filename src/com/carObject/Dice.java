0b3cfde6072ea4e7d473ad390f3f387db9bba1a6
Joel Olivero <joelolivero@outlook.com>
2020-10-16 19:42:56 -0400

    fixed rolling the correct die

--- src/com/carObject/Dice.java
+++ src/com/carObject/Dice.java
@@ -4,33 +4,30 @@ package com.carObject;
 
 import java.util.*;
 
-public class Dice extends Die {
+public class Dice {
     private int numberDice;
     public int rounds = 1;
     private static Scanner scan = new Scanner(System.in);
     public List<Die> dice = new ArrayList<Die>();
-    public List<Integer> dieList = new ArrayList<Integer>();
 
     public Dice(int numberDice, int rounds) {
         this.numberDice = numberDice;
         this.rounds = rounds;
-        beginGame();
     }
 
     private void assignValue() {
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 5; i++) {
             dice.add(new Die());
             dice.get(i).roll();
         }
         rounds++;
         System.out.println("Round " + rounds);
-        displayScore();
-        }
+    }
 
-        public void reRoll(int numberOfDice) {
-            Die die= dice.get(numberOfDice);
-            die.roll();
-        }
+    public void reRoll(int numberOfDice) {
+        Die die = dice.get(numberOfDice - 1);
+        die.roll();
+    }
 
     public void displayScore() {
        for (int j = 0; j < 5; j++) {
@@ -40,43 +37,41 @@ public class Dice extends Die {
     }
 
     public void userInteraction() {
-        while (rounds < 4) {
+        while (rounds < 3) {
+            displayScore();
             System.out.println("Input the dice you would like to reroll");
             String input = scan.nextLine();
             String[] numArray = input.split(" ");
             rounds++;
             for (String number : numArray) {
-                int choices = Integer.parseInt(number);
-                reRoll(choices);
-                displayScore();
-                System.out.println(rounds);
+                int choice = Integer.parseInt(number);
+                reRoll(choice);
             }
-            reset();
+          if(rounds == 3) {  displayScore(); reset(); }
         }
     }
 
     public void reset() {
         System.out.println("Would you like to try again? yes or no?");
         String input = scan.nextLine();
-        if(input == "yes") {
+        if(input.equals("yes")) {
             rounds = 1;
+        }else if (input.equals("no")) {
+            System.out.println("Thanks for playing");
+        }
+        else {
+            System.out.println("Please input a valid response");
         }
     }
 
     public void beginGame() {
         assignValue();
         userInteraction();
-
     }
 
         public int getDieValue (int pos){
             return dice.get(pos).getValue();
     }
-
-    public String toString(){
-        return Integer.toString(getValue());
-    }
-
 }
 
 //    public int getDieValue(int numberDice)
--- src/com/carObject/Main.java
+++ src/com/carObject/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Die die = new Die();
         Dice myRoll = new Dice(6, 0);
-        }
+        myRoll.beginGame();
     }
+}
 
